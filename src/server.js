af8746fdcbba537ee7b17cd5c17ec96a39c67258
reflash407 <86422079+reflash407@users.noreply.github.com>
2021-08-07 21:41:06 +0900

    chapter4 router success

--- src/server.js
+++ src/server.js
@@ -1,35 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+
+import globalRouter from "./routers/golbalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 5000;
 
 const app = express();
-
-const logger = (req,res,next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    console.log("Allowed, you may continue.")
-    next();
-};
-
-const handlehome = (req,res)=>{
-    return res.send("i love middlewares");
-};
-
-const handleProtected = (req,res) => {
-    return res.send("welcome to the protected loundge.");
-};
-
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handlehome);
-app.get("/protected",handleProtected);
+
+
+
+app.use("/",globalRouter);
+app.use("/video",videoRouter);
+app.use("/user",userRouter);
 
 const handleListening = () => 
     console.log(`Server listenting on port http://localhost:${PORT} ★`);
